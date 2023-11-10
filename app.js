af8bdacc675f597c0eba8d0000b3e21dd2c46402
Luke Hazelton <lukehaz35@gmail.com>
2023-11-10 00:23:49 -0800

    adding sanity check

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 80;
 // root route
 app.get('/', (req, res) => {
   const jObject = {
-    message: `My name is Luke Hazelton :)`,
+    message: `My name is Luke Hazelton SANITY CHECK`,
     timestamp: Date.now(),
   };
   res.json(jObject);
